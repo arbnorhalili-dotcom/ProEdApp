14cac4b3ec9f5982f3f465e3aab6d8b70d871d12
ArbnorHaliliUP <arbnor.halili@uni-pr.edu>
2024-01-31 19:50:29 +0100

    -

--- app/src/main/java/com/proed/proedapp/MainActivity.java
+++ app/src/main/java/com/proed/proedapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.email_message);
+        setContentView(R.layout.dialer_layout);
     }
 }
