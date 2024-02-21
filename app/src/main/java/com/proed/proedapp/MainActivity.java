f7acabf4696dc1e517131a7790d49e479dcc0fe0
arbnorhalili <arbnor.halili@uni-pr.edu>
2024-02-21 18:18:16 +0100

    -

--- app/src/main/java/com/proed/proedapp/MainActivity.java
+++ app/src/main/java/com/proed/proedapp/MainActivity.java
@@ -2,13 +2,60 @@ package com.proed.proedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvWelcome;
+    EditText etUsername, etPassword;
+    Button btnLogin, btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialer_layout);
+        setContentView(R.layout.login_relative_layout);
+
+        /*tvWelcome = findViewById(R.id.tvWelcome);
+        tvWelcome.setText("Mire se erdhet...");
+        tvWelcome.setTextColor(getColor(android.R.color.holo_red_dark));
+        tvWelcome.setTextSize(50);*/
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                if(username.equals("admin") && password.equals("admin"))
+                {
+                    Toast.makeText(MainActivity.this,
+                            "Kredencialet jane ne rregull!",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,
+                            "Kredencialet jane GABIM!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
